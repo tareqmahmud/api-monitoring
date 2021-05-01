2778fe15fb503ccd8a5228c336ea5c893b366107
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-05-01 19:29:54 +0600

    Add feature to update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -106,7 +106,61 @@ controller._users.post = (req, callback) => {
 
 // Put method
 controller._users.put = (req, callback) => {
+    // Validate the input body data
+    const firstName = typeof (req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+    const lastName = typeof (req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+    const phone = typeof (req.body.phone) === 'string' && req.body.phone.trim().length === 11 ? req.body.phone : false;
+    const password = typeof (req.body.password) === 'string' && req.body.password.trim().length > 0 ? req.body.password : false;
+    const toAgreement = typeof (req.body.toAgreement) === 'boolean' ? req.body.toAgreement : false;
 
+    if (phone) {
+        // Only update data if user at least provide one data except phone and agreement
+        if (firstName || lastName || password) {
+            libData.read('users', phone, (err, user) => {
+                if (!err) {
+                    // Retrieve existing user
+                    const userObject = {...utilities.parseJson(user)}
+
+                    // Only update userObject which is available
+                    if (firstName) {
+                        userObject.firstName = firstName;
+                    }
+
+                    if (lastName) {
+                        userObject.lastName = lastName;
+                    }
+
+                    if (password) {
+                        userObject.password = utilities.hash(password);
+                    }
+
+                    // Finally update the data
+                    libData.update('users', phone, userObject, (err, user) => {
+                        if (!err) {
+                            // Return the updated user
+                            delete userObject.password;
+
+                            callback(200, {
+                                user: userObject
+                            })
+                        } else {
+                            callback(500, {
+                                error: 'Something wrong in server!'
+                            })
+                        }
+                    })
+                } else {
+                    callback(400, {
+                        error: 'Invalid phone number'
+                    })
+                }
+            })
+        }
+    } else {
+        callback(400, {
+            error: 'Please input all the required fields'
+        })
+    }
 }
 
 // Update method
